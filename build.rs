d0e17955dea836cc99e19a978f711408e41c68a4
Martin Habovstiak <martin.habovstiak@gmail.com>
2022-02-12 00:01:15 +0100

    Turn off `tm_zone`
    The pointer has undocumented lifetime so it's a useless footgun.
    Turning it off seems safer. It's supposed to be optional anyway.

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() {
     cc::Build::new()
         .file("c_lib/localtime.c")
+        // tm_zone has undocumented lifetime so better turn it off
+        .define("NO_TM_ZONE", None)
+        .define("STD_INSPIRED", None)
         .warnings(std::env::var_os("RL_LOCALTIME_WARN").is_some())
         .compile("rllocaltime");
 }
